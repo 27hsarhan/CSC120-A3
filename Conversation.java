41068ce3c667cb9870817068a2dde1c4fbe4c812
27hsarhan <158182999+27hsarhan@users.noreply.github.com>
2024-03-06 16:12:49 -0500

    Add files via upload

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 class Conversation {
 
   public static void main(String[] arguments) {
+    ArrayList<String> transcript = new ArrayList<String>();
     // You will start the conversation here.
+    //nRounds=input("How many rounds?: ")
+    System.out.print("How many rounds?");
+    transcript.add("How many rounds?");
+    Scanner input = new Scanner(System.in);
+    int nRounds = input.nextInt();
+    input.nextLine(); //clearing new line character
+    transcript.add(String.valueOf(nRounds));
+    System.out.println("Hi there! What's on your mind?"); 
+    transcript.add("Hi there! What's on your mind?");
+    for (int i = 0; i < nRounds; i++){
+       String user_input = input.nextLine();
+      transcript.add(user_input);
+      System.out.println("Uh-huh...");
+      transcript.add("Uh-huh...");
+
+
+      //String strArray[];
+        //String inputArray = new strArray[];
+      // pass lines through a new array, print the arry
+      //System.out.println("Transcript:" + )
+      //if input contains i:
+        //print(f{"you" + input})
+      //elif input contains am:
+        //print(f{"are" + input})
+    }
+    System.out.println("Goodbye.");
+    transcript.add("Goodbye.");
+    System.out.println(transcript);
   }
 }
